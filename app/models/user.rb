397250fc6de9fce0e0fbe0d7c552ad629260b6f6
RuutoRuuto <n56g034y@gmail.com>
2021-02-18 19:14:07 +0900

    バリデーションの編集とネスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name
-    validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナでの入力が必要。"}
-    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナでの入力が必要。"}
     validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナでの入力が必要。"} do
+      validates :kana_last_name 
+      validates :kana_first_name
+    end
   end
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]/i, message: "は半角英数字混合での入力が必要。"}
 end
