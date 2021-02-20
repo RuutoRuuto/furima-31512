d57eadc37a3422b28ccf584cf946cd4e58800ace
RuutoRuuto <n56g034y@gmail.com>
2021-02-20 19:46:25 +0900

    active hash部分を表示とそのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  
+  with_options presence: true do
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :charge_type_id
+      validates :prefecture_id
+      validates :shipment_date_id
+    end
+  end
 end
