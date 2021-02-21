c4b4b38aa12001c659d8b5781a11a13d0f48961d
RuutoRuuto <n56g034y@gmail.com>
2021-02-21 13:04:42 +0900

    itemバリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,16 @@ class Item < ApplicationRecord
   belongs_to :category
 
   with_options presence: true do
-    with_options numericality: { other_than: 1 } do
+    validates :image
+    validates :name
+    validates :description
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
       validates :condition_id
       validates :charge_type_id
       validates :prefecture_id
       validates :shipment_date_id
     end
-    validates :name
-    validates :description
     validates :price, format: { with: /[1-9]|[1-9][0-9]{2,6}/, message: "は300~9,999,999の範囲の半角数字での入力が必要"}
   end
 end
