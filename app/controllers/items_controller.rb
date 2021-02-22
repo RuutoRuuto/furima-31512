640d9f4010aca06343f45577e6c78532746f8933
RuutoRuuto <n56g034y@gmail.com>
2021-02-22 21:42:12 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item =Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -18,9 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  private 
-  def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :charge_type_id, :prefecture_id, :shipment_date_id, :price).merge(user_id: current_user.id)
-  end
+  private
 
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :charge_type_id, :prefecture_id,
+                                 :shipment_date_id, :price).merge(user_id: current_user.id)
+  end
 end
