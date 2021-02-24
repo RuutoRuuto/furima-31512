e64634f9f359b72a9fedbeca77a8e063ff486bd9
RuutoRuuto <n56g034y@gmail.com>
2021-02-24 22:50:51 +0900

    商品詳細表示機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
     @item = Item.order("created_at DESC")
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
