b7c35b18ddde40673023c24fe79c51392be906fc
RuutoRuuto <n56g034y@gmail.com>
2021-03-04 19:25:10 +0900

    orderテーブルへの保存を確認

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,24 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render 'index'
+    end
   end
-  
+
+  private
+
+  def order_params
+    params.permit.merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
