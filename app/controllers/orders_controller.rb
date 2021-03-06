96f740c55f544bc8b0500cbbd61dc86ace75e747
RuutoRuuto <n56g034y@gmail.com>
2021-03-06 23:09:18 +0900

    カードで購入処理と各ページ遷移部分

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :check_same_user
+  before_action :check_sold_item
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render 'index'
     end
   end
@@ -19,7 +22,29 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_num).merge(
+      user_id: current_user.id, item_id: params[:item_id], price: @item.price, token: params[:token]
+    )
   end
 
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def check_same_user
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def check_sold_item
+    redirect_to root_path if (Order.find_by item_id: @item.id) != nil
+  end
 end
---
+++ spec/factories/order_address.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture_id { 5 }
+    city { '東京都' }
+    address { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_num { '12345678912' }
+    price { 2000 }
+    token { 'tok_abcdefghijk00000000000000000' }
+  end
+end
